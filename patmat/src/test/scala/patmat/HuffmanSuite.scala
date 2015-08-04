7bd89839ddb4b7777e3f06705100587176a84463
Daniel Fulop <daniel.fulop@hotmail.com>
2015-08-04 23:29:17 +0200

    Initial commit

---
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,69 @@
+package patmat
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import patmat.Huffman._
+
+@RunWith(classOf[JUnitRunner])
+class HuffmanSuite extends FunSuite {
+  trait TestTrees {
+    val t1 = Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5)
+    val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
+  }
+
+  trait TestLists {
+    val emptyList = List()
+    val list = List('a', 'b', 'a')
+  }
+
+  test("weight of a larger tree") {
+    new TestTrees {
+      assert(weight(t1) === 5)
+    }
+  }
+
+  test("chars of a larger tree") {
+    new TestTrees {
+      assert(chars(t2) === List('a','b','d'))
+    }
+  }
+
+  test("frequency of an empty list") {
+    new TestLists {
+      assert(times(emptyList) === List())
+    }
+  }
+
+  test("frequency of a non-empty list") {
+    new TestLists {
+      val result = times(list)
+      val aPair = ('a', 2)
+      val bPair = ('b', 1)
+      assert(result.length === 2)
+      assert(times(list).exists(pair => pair === aPair))
+      assert(times(list).exists(pair => pair === bPair))
+    }
+  }
+
+  test("string2chars(\"hello, world\")") {
+    assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
+  test("makeOrderedLeafList for some frequency table") {
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
+  }
+
+  test("combine of some leaf list") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assert(combine(leaflist) === List(Fork(Leaf('e',1),Leaf('t',2),List('e', 't'),3), Leaf('x',4)))
+  }
+
+  test("decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+}
